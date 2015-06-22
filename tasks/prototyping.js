4012d3020343e2695bacaa34f0bd1db31cccdf2e
Matthew Daly <matthewbdaly@gmail.com>
2015-06-22 20:52:04 +0100

    Added some code

--- tasks/prototyping.js
+++ tasks/prototyping.js
@@ -14,36 +14,113 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('prototyping', 'A plugin for prototyping websites with Handlebars, Markdown and Bootstrap', function() {
+
+    // Declare variables
+    var parseUrl = require('url'),
+      MarkedMetadata = require('meta-marked'),
+      Handlebars = require('handlebars');
+    
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
       separator: ', '
     });
+    options.domain = parseUrl.parse(options.data.url).hostname;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    // Create a helper for joining arrays
+    Handlebars.registerHelper('join', function (items) {
+        return items.join(',');
+    });
+
+    // Create a helper for capitalizing the first letter of words
+    Handlebars.registerHelper('capitalize', function (item, options) {
+        // Break string into parts
+        var reservedWords, word, words = item.split(' ');
+
+        // Define reserved words
+        reservedWords = [
+            'a',
+            'and',
+            'in',
+            'from',
+            'to',
+            'the',
+            'for',
+            'on',
+            'of'
+        ];
+
+        // If is not one of the reserved words
+        for (word in words) {
+            // Skip the first word
+            if (word === 0) {
+                continue;
+            }
+
+            // Search for word in reservedWords
+            if (reservedWords.indexOf(words[word]) === -1) {
+                // Is word already capitalized?
+                if (words[word].toUpperCase() !== words[word]) {
+                    words[word] = words[word].charAt(0).toUpperCase() + words[word].slice(1).toLowerCase();
+                }
+            }
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        return words.join(' ');
+    });
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+    // Set options
+    var mdoptions = {
+      gfm: true,
+      tables: true,
+      smartLists: true,
+      smartypants: true
+    };
+    MarkedMetadata.setOptions(mdoptions);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    // Get matching files
+    pages = grunt.file.expand(options.src.pages + '*.md', options.src.pages + '*.markdown');
+
+    // Get Handlebars templates
+    pageTemplate = Handlebars.compile(grunt.file.read(options.template.page));
+    notFoundTemplate = Handlebars.compile(grunt.file.read(options.template.notfound));
+
+    // Generate pages and add them to the index
+    var file;
+    pages.forEach(function (file) {
+        // Convert it to Markdown
+        var content = grunt.file.read(file);
+        var md = new MarkedMetadata(content);
+        var mdcontent = md.html;
+        var meta = md.meta;
+
+        // Set permalink
+        var permalink = '/' + (file.replace(options.src.pages, '').replace('.markdown', '').replace('.md', ''));
+        var path = options.www.dest + permalink;
+
+        // Render the Handlebars template with the content
+        var data = {
+            year: options.year,
+            data: options.data,
+            domain: options.domain,
+            canonical: options.data.url + permalink + '/',
+            path: path,
+            meta: {
+                title: meta.title.replace(/"/g, ''),
+                date: meta.date
+            },
+            post: {
+                content: mdcontent,
+                rawcontent: content
+            },
+            recent_posts: recent_posts
+        };
+        output = pageTemplate(data);
+
+        // Write page to destination
+        grunt.file.mkdir(path);
+        grunt.file.write(path + '/index.html', output);
     });
   });
 
